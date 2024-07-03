874d606067a96e60cc82ea17cfdbb45549b4433f
joshiabhishek0392 <162970629+joshiabhishek0392@users.noreply.github.com>
2024-07-03 17:25:45 +0530

    blogs api uploaded

--- src/main/java/com/sunbeam/entities/User.java
+++ src/main/java/com/sunbeam/entities/User.java
@@ -1,83 +1,173 @@
 package com.sunbeam.entities;
 
-import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-public class User {
-	private int id;
+import javax.persistence.*;
+
+import com.sunbeam.value_types.AdhaarCard;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+	@Column(name = "first_name", length = 25) // col name , varchar(25)
 	private String firstName;
+	@Column(name = "last_name", length = 25) // col name , varchar(25)
 	private String lastName;
+	@Column(length = 20, unique = true) // varchar(20), unique constraint
 	private String email;
+	@Column(length = 25, nullable = false) // NOT NULL
 	private String password;
-	private Date birth;
-	private int status;
-	private String role;
+	private LocalDate dob;
+	@Column(name = "reg_amount")
+	private double regAmount;
+	@Enumerated(EnumType.STRING) // col type : varchar
+	@Column(length = 20)
+	private Role role;
+	@Lob // => large object , col type : longblob
+	private byte[] image;
+	// User 1-->1 Address , example of uni dir one-one association between the
+	// entities
+	//one , child , owning
+	@OneToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name = "address_id")
+	private Address userAddress;
+	// entity -> composite value type
+	@Embedded // optional annotation
+	private AdhaarCard card;
+	// entity -> collection of value types
+	@ElementCollection
+	private List<String> languages = new ArrayList<>();
+
+	// def ctor is MANDATORY
 	public User() {
-		super();
+		// TODO Auto-generated constructor stub
 	}
-	public User(int id, String firstName, String lastName, String email, String password, Date birth, int status,
-			String role) {
+	
+
+	// user reg
+	public User(String firstName, String lastName, String email, String password, LocalDate dob, double regAmount,
+			Role role) {
 		super();
-		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
-		this.birth = birth;
-		this.status = status;
+		this.dob = dob;
+		this.regAmount = regAmount;
 		this.role = role;
 	}
-	public int getId() {
-		return id;
+
+	// add overloaded ctor for ctor expression
+	public User(String firstName, String lastName, LocalDate dob) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
 	}
-	public void setId(int id) {
-		this.id = id;
+	
+
+	public User(String firstName, String lastName, LocalDate dob, double regAmount) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.regAmount = regAmount;
 	}
+
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public Date getBirth() {
-		return birth;
+
+	public LocalDate getDob() {
+		return dob;
 	}
-	public void setBirth(Date birth) {
-		this.birth = birth;
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
 	}
-	public int getStatus() {
-		return status;
+
+	public double getRegAmount() {
+		return regAmount;
 	}
-	public void setStatus(int status) {
-		this.status = status;
+
+	public void setRegAmount(double regAmount) {
+		this.regAmount = regAmount;
 	}
-	public String getRole() {
+
+	public Role getRole() {
 		return role;
 	}
-	public void setRole(String role) {
+
+	public void setRole(Role role) {
 		this.role = role;
 	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	public Address getUserAddress() {
+		return userAddress;
+	}
+
+	public void setUserAddress(Address userAddress) {
+		this.userAddress = userAddress;
+	}
+
+	public AdhaarCard getCard() {
+		return card;
+	}
+
+	public void setCard(AdhaarCard card) {
+		this.card = card;
+	}
+
+	public List<String> getLanguages() {
+		return languages;
+	}
+
+	public void setLanguages(List<String> languages) {
+		this.languages = languages;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+ ", password=" + password + ", birth=" + birth + ", status=" + status + ", role=" + role + "]";
+		return "User [id=" + getId() + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", dob=" + dob + ", regAmount=" + regAmount + ", role=" + role + "]";
 	}
-	
 }
